cb2ebb03957df4c9131b8938cb1ed143aa413e06
Victor Otieno <victorotieno598@gmail.com>
2021-09-28 12:09:23 +0300

    Add redux logger for debugging

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { rootReducer } from './books/books';
 
 export const store = createStore(
   rootReducer,
   {},
-  applyMiddleware(thunk),
+  applyMiddleware(thunk, logger),
 );
