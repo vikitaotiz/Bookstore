ee459740033723c7c4d0d17c584dffd025983c45
Victor Otieno <victorotieno598@gmail.com>
2021-09-29 18:28:29 +0300

    Add notification area

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,34 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const Navbar = () => (
-  <div className="navbar navbar-expand-lg navbar-light bg-white mb-5 my-0">
-    <div className="container-fluid">
-      <a className="navbar-brand primary-color fw-bold" href="/">
-        <i className="fas fa-book-reader mr-3" />
-        {' '}
-        Bookstore CMS
-      </a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/" exact>Books</NavLink>
-          </li>
-          <li><NavLink className="nav-link" to="/categories" exact>Categories</NavLink></li>
-        </ul>
+const Navbar = () => {
+  const books = useSelector((state) => state.books.length);
+  return (
+    <div className="navbar navbar-expand-lg navbar-light bg-white mb-5 my-0">
+      <div className="container-fluid">
+        <a className="navbar-brand primary-color fw-bold" href="/">
+          <i className="fas fa-book-reader mr-3" />
+          {' '}
+          Bookstore CMS
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" exact>Books</NavLink>
+            </li>
+            <li><NavLink className="nav-link" to="/categories" exact>Categories</NavLink></li>
+          </ul>
+          <i className="fas fa-user-alt primary-color">
+            {' '}
+            { books}
+          </i>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
